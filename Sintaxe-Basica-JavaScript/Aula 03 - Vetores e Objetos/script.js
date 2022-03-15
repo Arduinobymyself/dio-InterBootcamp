6b71e27bfcf111e1474a6fef8aa574f2c8339b74
arduinobymyself <arduinobymyself@gmail.com>
2022-03-14 22:08:14 -0300

    alteração aula 3

--- Sintaxe-Basica-JavaScript/Aula 03 - Vetores e Objetos/script.js
+++ Sintaxe-Basica-JavaScript/Aula 03 - Vetores e Objetos/script.js
@@ -6,7 +6,7 @@ let array = ['string', 1, true];
 console.log(array); */
 
 // como um array pode guardar difentes valores inclusive outros arrays
-//let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+// let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
 
 /* // como acessar o índice de um array
 console.log(array[0]); */
@@ -45,24 +45,28 @@ console.log(array[0]); */
 //console.log(novoArray);
 
 
-/* // o que são objetos
+// o que são objetos
 // como declarar um objeto
-let object = { propriedade1: 'valor1', propriedade2: 'valor2' }
+//let object = { propriedade1: 'valor1', propriedade2: 'valor2' }
 // deve ser declarado entre chaves "{}";
-console.log(object); */
+//console.log(object);
 
 // assim como arrays as propriedades de objetos podem guardar quaisquer valores
-// let object = { string: 'string', number: 1, boolean: true, array: ['array'], objectInterno: { objetoInterno: 'objeto Interno' } };
+// let object = { string: 'string', 
+//                 number: 1, 
+//                 boolean: true, 
+//                 array: ['array'], 
+//                 objectInterno: { objetoInterno: 'objeto Interno' } };
 
-/* // como acessar propriedades de objetos
+ // como acessar propriedades de objetos
 // usando ponto "."
-console.log(object.objectInterno); */
+//console.log(object.objectInterno);
 
 // manipulando objeto
 // desestruturando objeto
-/* let string = object.string;
-let number = object.number;
-let boolean = object.boolean; */
+//let string = object.string;
+//let number = object.number;
+//let boolean = object.boolean;
 
 
 // desestruturando usando chaves
